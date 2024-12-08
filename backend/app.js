021ec76a5358b2cdcac184b97fef7bda091be2e0
DeboraRivas1805 <00059922@uca.edu.sv>
2024-12-08 16:38:29 -0600

    Versión Final

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,7 @@ const attendeeRoutes = require('./routes/attendeeRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 const { errorHandler } = require('./utils/errorHandler');
 
-// Inicializar Express
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware global
 app.use(cors());
@@ -27,17 +25,4 @@ app.use('/api/comments', commentRoutes);
 // Middleware global de manejo de errores
 app.use(errorHandler);
 
-// Probar conexión a la base de datos
-db.authenticate()
-  .then(() => console.log("Conexión exitosa a la base de datos"))
-  .catch((error) => console.error("Error al conectar a la base de datos:", error));
-
-// Rutas
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/events", require("./routes/eventRoutes"));
-app.use("/api/rsvp", require("./routes/rsvpRoutes"));
-app.use("/api/comments", require("./routes/commentRoutes"));
-
-
-// Iniciar servidor
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+module.exports = app;
